fd3a4b20be25f5079cd280197e1ef56cb4c8f1ff
Juju <57468970+juliacardillo@users.noreply.github.com>
2021-03-30 23:59:16 -0400

    reupload

---
+++ project2/src/receiver.py
@@ -0,0 +1,32 @@
+# ----- receiver.py -----
+
+#!/usr/bin/env python
+
+from socket import *
+import sys
+import select
+import time
+
+#host="10.0.0.2"
+host = ''
+port = int(sys.argv[1])
+s = socket(AF_INET,SOCK_DGRAM)
+s.bind((host,port))
+
+addr = ('',port)
+buf=1024
+t_start = time.time()
+#f = open("received.txt",'wb')
+
+data,addr = s.recvfrom(buf)
+
+try:
+    while(data):
+        data = data.decode()
+        sys.stdout.write(data)
+        s.settimeout(2)
+        data,addr = s.recvfrom(buf)
+except timeout:
+    
+    s.close()
+    print("File Downloaded")
