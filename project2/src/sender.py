e7d74a3e4fb9ce2b89e56231606298f5194c7b5e
Muirrin O'Connell <35041844+Muirrin@users.noreply.github.com>
2021-04-09 00:00:13 -0400

    Update sender.py

--- project2/src/sender.py
+++ project2/src/sender.py
@@ -6,17 +6,18 @@ import sys
 import time 
 
 numBytes = 0
+message = ''
 buf =1024 
 s = socket(AF_INET,SOCK_DGRAM)
 host =sys.argv[1]
 port = int(sys.argv[2])
 addr = (host,port)
 data = ''
-
+sequence = 0
 
 def packets(data):
   content = bytearray(data.encode())
-  packets = [content[i:i+buf] for i in range(0,len(content),buf)]
+  packets = [content[i:i+buf-1] for i in range(0,len(content),buf)]
   return packets
 
 #getting the data
@@ -24,17 +25,49 @@ for line in sys.stdin:
   data = data + line
  
 mypackets = packets(data)
+
+#wrap packets in header
+for packet in mypackets:
+  if sequence == 10:
+    sequence = 0
+  seqnum = bytearray(str(sequence).encode())
+  packet += seqnum
+  sequence += 1
+
 i = 0
 #make timer 
 t_start = time.time()
 
 while (i<len(mypackets)):
+    #must implement try except 
+  s.settimeout(2)
+  try:
     if(s.sendto(mypackets[i],addr)):
-        print("sending ...")
-        #data = f.read(buf)
-        numBytes += len(mypackets[i])
-        i = i+1
-
+      print("sending ...")
+      #data = f.read(buf)
+      #receive ACK, wait if not
+      #try to receive message
+      message,addr = s.recvfrom(buf)
+      message = message.decode()
+      print('i=' + str(i))
+      print(message)
+      numBytes += len(mypackets[i])
+      i = i+1
+          
+  except Exception as stimeout:
+      if timeout occurs, retransmit the packet 
+      if(len(message)==0):
+        j = 0
+      else:
+        seqnum = message[0]
+       j = int(seqnum) +1
+      print('j= ' + str(j))
+      print("resending ...")
+      #s.sendto(mypackets[j],addr)
+      #message,addr = s.recvfrom(buf)
+      #message = message.decode()
+      i = j+1
+  
 s.close()
 
 t_end = time.time()
